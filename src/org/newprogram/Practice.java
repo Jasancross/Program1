ddf89ff11c0f8ddaecee0b3c5ac5d217110d34d0
Jasancross <jasancross@gmail.com>
2022-08-27 12:50:15 +0530

    message

--- src/org/newprogram/Practice.java
+++ src/org/newprogram/Practice.java
@@ -1,31 +1,26 @@
 package org.newprogram;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Practice {
 
 	public static void main(String[] args) {
+		
+	
+		int number = 10203040;
+		
+		String numbertostring = number+"";
+		
+		String replace = numbertostring.replace("0", "");
+		
+		int parseInt = Integer.parseInt(replace);
+		
+		System.out.println(parseInt);
+		
 
-		Scanner scan = new Scanner(System.in);
-
-		System.out.println("Print an alphabet");
-
-		String Alphabet = scan.nextLine();
-
-		if(Country.equals("a")) {
-
-			System.out.println(Country +" Its a Developing country");	
-		}
-		else if (Country.equals("America")) {
-
-			System.out.println(Country +" Its a Developed country");
-		}
-		else if (Country.equals("Africa")) {
-		System.out.println(Country +" Poor Country");
-		}
-		else {
-			System.out.println("Not Mentioned");
-		}
-			
+		
+		
 		
 	}}
